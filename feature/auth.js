b3dc44efae9f5c40e79a3cf8d0b0835abe45c5d6
clai <clai-@hotmail.com>
2017-05-03 14:05:39 -0700

    authenticationworkingish

--- feature/auth.js
+++ feature/auth.js
@@ -1,4 +1,15 @@
-firebase.initializeApp(config);
+(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBWQ0DKA_LDHLrsNEUkFRxh8Dr9rKKnTVA",
+    authDomain: "plentyofthyme-c301a.firebaseapp.com",
+    databaseURL: "https://plentyofthyme-c301a.firebaseio.com",
+    projectId: "plentyofthyme-c301a",
+    storageBucket: "plentyofthyme-c301a.appspot.com",
+    messagingSenderId: "1087388674603"
+  };
+  firebase.initializeApp(config);
+
 // Get elements
 const txtEmail = document.getElementById('txtEmail');
 const txtPassword = document.getElementById('txtPassword');
@@ -37,8 +48,10 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
   if(firebaseUser) {
     console.log(firebaseUser);
     btnLogout.classList.remove('hide');
+    window.location.href ='landing.html';
   } else {
     console.log('not logged in');
     btnLogout.classList.add('hide');
   }
 });
+}());
