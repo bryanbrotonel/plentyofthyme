1aeaf474ffca54ae4fbaba6e5a827d0f2b2c512b
clai <clai-@hotmail.com>
2017-05-04 15:02:01 -0700

    listworking

--- feature/grocerylist.js
+++ feature/grocerylist.js
@@ -10,32 +10,48 @@
     };
     firebase.initializeApp(config);
 
+    var count = 0;
     // Get elements
     const itemName1 = document.getElementById('item1name');
     const itemPrice1 = document.getElementById('item1price');
     const proObject = document.getElementById('object');
     const btnSubmit = document.getElementById('btnSubmit');
+    const select = document.getElementById('select');
+    const itemDate1 = document.getElementById('item1date');
+    const itemSelect1 = document.getElementById('item1select');
 
     // Create references
     const dbRefObject = firebase.database().ref().child('object');
 
     // Sync object changes
-    dbRefObject.on('value', snap => {
-      preObject.innerText = JSON.stringify(snap.val(), null, 3);
-      console.log(snap.val());
-    });
+    // dbRefObject.on('value', snap => {
+    //   preObject.innerText = JSON.stringify(snap.val(), null, 3);
+    //   console.log(snap.val());
+    // });
 
-  function addItem(itemName, itemPrice) {
-  firebase.database().ref().set({
+  function addItem(itemName, itemPrice, itemDate, itemCat, amount) {
+  firebase.database().ref().child('items').push({
     name: itemName,
-    price: itemPrice
+    price: itemPrice,
+    date: itemDate,
+    category: itemCat,
+    quantity: amount
   });
+
   }
 
   btnSubmit.addEventListener('click', e => {
     const iName = itemName1.value;
     const iPrice = itemPrice1.value;
-    addItem(iName, iPrice);
+    const iDate = itemDate1.value;
+    const cat = itemSelect1.value;
+    var iSelect = select.value;
+    addItem(iName, iPrice, iDate, cat, iSelect);
+    document.getElementById('item1name').value = "";;
+    document.getElementById('item1price').value = "";
+    document.getElementById('select').value = "";
+    document.getElementById('item1date').value = "";
+    document.getElementById('item1select').value = "";
   });
 
 }());
