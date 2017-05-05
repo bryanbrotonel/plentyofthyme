6ed0caacf8f8c496d67bdeaee5be6259129438f6
clai <clai-@hotmail.com>
2017-05-05 11:16:40 -0700

    notsure

---
+++ js/displaylist.js
@@ -0,0 +1,20 @@
+(function() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBWQ0DKA_LDHLrsNEUkFRxh8Dr9rKKnTVA",
+        authDomain: "plentyofthyme-c301a.firebaseapp.com",
+        databaseURL: "https://plentyofthyme-c301a.firebaseio.com",
+        projectId: "plentyofthyme-c301a",
+        storageBucket: "plentyofthyme-c301a.appspot.com",
+        messagingSenderId: "1087388674603"
+    };
+    firebase.initializeApp(config);
+
+    const preObject = document.getElementById('object');
+    const dbRefList = preOjbect.child(items);
+
+    dbRefObject.on('value', snap => {
+      preObject.innerText = JSON.stringify(snap.val(), null, 3);
+    });
+
+}());
