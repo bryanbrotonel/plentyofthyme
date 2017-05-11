0739c0f4393382db879afe0d9a90f48c7f4fe6b5
clai <clai-@hotmail.com>
2017-05-11 09:57:31 -0700

    fixednav

--- feature/logout.js
+++ feature/logout.js
@@ -1,8 +1,14 @@
 (function() {
   const btnLogout = document.getElementById('btnLogout');
+  const btnLogout1 = document.getElementById('btnLogout1');
 
-    btnLogout.addEventListener('click', e => {
-        firebase.auth().signOut();
-        Materialize.toast('Successfully logged out!', 4000);
-    });
+  btnLogout1.addEventListener('click', e => {
+      firebase.auth().signOut();
+      Materialize.toast('Successfully logged out!', 4000);
+  });
+
+  btnLogout.addEventListener('click', e => {
+      firebase.auth().signOut();
+      Materialize.toast('Successfully logged out!', 4000);
+  });
 }());
