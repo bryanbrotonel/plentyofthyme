17308941db7e72a89dd6fec3bf37805c247f3570
clai <clai-@hotmail.com>
2017-05-11 16:23:30 -0700

    FIXED THE SHIT OUT OF LANDING PLS DONT TOUCH UNLESS YOU KNOW FOR SURE WHAT YOU ARAE DOING PLS JFC

--- feature/landingnav.js
+++ feature/landingnav.js
@@ -1,4 +1,4 @@
-
+// lg view
 const btnCart = document.getElementById('btnCart');
 const btnStats = document.getElementById('btnStats');
 const btnCalendar = document.getElementById('btnCalendar');
@@ -12,5 +12,39 @@ btnStats.addEventListener('click', e => {
 });
 
 btnCalendar.addEventListener('click', e => {
+  window.location.href='calendar.html';
+})
+
+// sm view
+const btnCart_sm = document.getElementById('btnCart_sm');
+const btnStats_sm = document.getElementById('btnStats_sm');
+const btnCalendar_sm = document.getElementById('btnCalendar_sm');
+
+btnCart_sm.addEventListener('click', e => {
+  window.location.href='groceryList.html';
+});
+
+btnStats_sm.addEventListener('click', e => {
+  window.location.href='stats.html';
+});
+
+btnCalendar_sm.addEventListener('click', e => {
+  window.location.href='calendar.html';
+})
+
+// xs view
+const btnCart_xs = document.getElementById('btnCart_xs');
+const btnStats_xs = document.getElementById('btnStats_xs');
+const btnCalendar_xs = document.getElementById('btnCalendar_xs');
+
+btnCart_xs.addEventListener('click', e => {
+  window.location.href='groceryList.html';
+});
+
+btnStats_xs.addEventListener('click', e => {
+  window.location.href='stats.html';
+});
+
+btnCalendar_xs.addEventListener('click', e => {
   window.location.href='calendar.html';
 })
