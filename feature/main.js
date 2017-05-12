4634f375f77a9977ad7b2b18e750d409c9ef96f5
bryanbrotonel <bryanbrotonel@live.ca>
2017-05-12 13:31:53 -0700

    fixed main.js

--- feature/main.js
+++ feature/main.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-// // Initialize Firebase
-// var config = {
-//   apiKey: "AIzaSyBWQ0DKA_LDHLrsNEUkFRxh8Dr9rKKnTVA",
-//   authDomain: "plentyofthyme-c301a.firebaseapp.com",
-//   databaseURL: "https://plentyofthyme-c301a.firebaseio.com",
-//   projectId: "plentyofthyme-c301a",
-//   storageBucket: "plentyofthyme-c301a.appspot.com",
-//   messagingSenderId: "1087388674603"
-// };
-//
-// firebase.initializeApp(config);
-//
-// firebase.auth().onAuthStateChanged(function(user) {
-//   if (user) {
-//     // User is signed in.
-//   } else {
-//     setTimeout(function() {
-//       window.location.href = 'index.html';
-//     }, 500);
-//   }
-// });
-=======
 <!--// Initialize Firebase
 var config = {
   apiKey: "AIzaSyBWQ0DKA_LDHLrsNEUkFRxh8Dr9rKKnTVA",
@@ -42,4 +19,3 @@ firebase.auth().onAuthStateChanged(function(user) {
     }, 500);
   }
 });-->
->>>>>>> b7f82f1ee8b084d376ebb3bca4b8ff912bdb6c10
