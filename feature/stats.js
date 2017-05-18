f4fbcff1ed531860a0bf68adaf657189ca6786d2
clai <clai-@hotmail.com>
2017-05-18 16:08:18 -0700

    stats update

--- feature/stats.js
+++ feature/stats.js
@@ -1,35 +1,40 @@
 // set variables
 // var values = [];
 var objects = [];
-var wasted = 0;
+var totalWasted = 0; // money wasted stat
+var dairyWasted = 0;
+var produceWasted = 0;
+var meatWasted = 0;
+var miscWasted = 0;
+var itemsWasted = 0;
 
 // access to firebase users
 firebase.auth().onAuthStateChanged(function(user) {
-    var database = firebase.database().ref('users/' + user.uid);
-    database.once('value', function(snapshot) {
-      i = 0;
-      if (snapshot.val() != null) {
-        snapshot.forEach(function(childSnapshot) {
-          childSnapshot.forEach(function(childChildSnapshot) {
-            console.log(i);
-            objects[i] = childChildSnapshot.val();
-            // values[i] = objects[i].name + objects[i].date;
-            i++;
-          });
+  var database = firebase.database().ref('users/' + user.uid);
+  database.once('value', function(snapshot) {
+    i = 0;
+    if (snapshot.val() != null) {
+      snapshot.forEach(function(childSnapshot) {
+        childSnapshot.forEach(function(childChildSnapshot) {
+          console.log(i);
+          objects[i] = childChildSnapshot.val();
+          // values[i] = objects[i].name + objects[i].date;
+          i++;
         });
-      } else {
+      });
+    } else {
+      ;
+    }
 
-      }
+    i = 0;
 
-      console.log(i);
-      i = 0;
-      console.log(i);
-      for (i = 0; i < objects.length; i++) {
-        if (objects[i].expired.equals('yes')) {
-          wasted += objects[i].price;
-        }
+    // total money wasted
+    for (i = 0; i < objects.length; i++) {
+      if (objects[i].expired === ('yes')) {
+        totalWasted += objects[i].price;
       }
-    });
+    }
   });
+});
 
-console.log(wasted);
+console.log(totalWasted);
