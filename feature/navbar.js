063a25aeb20cedd1425f351aa6f726d89117566d
clai <clai-@hotmail.com>
2017-05-18 22:49:14 -0700

    js fixes fresh fam

--- feature/navbar.js
+++ feature/navbar.js
@@ -8,50 +8,50 @@ $('.button-collapse').sideNav({
   draggable: true // Choose whether you can drag to open on touch screens
 });
 
-$(document).ready(function() {
-  var myNavBar = {
-    flagAdd: true,
-    elements: [],
-    init: function(elements) {
-      this.elements = elements;
-    },
-    add: function() {
-      if (this.flagAdd) {
-        for (var i = 0; i < this.elements.length; i++) {
-          document.getElementById(this.elements[i]).className += " fixed-theme";
-        }
-        this.flagAdd = false;
-      }
-    },
-    remove: function() {
-      for (var i = 0; i < this.elements.length; i++) {
-        document.getElementById(this.elements[i]).className =
-          document.getElementById(this.elements[i]).className.replace(/(?:^|\s)fixed-theme(?!\S)/g, '');
-      }
-      this.flagAdd = true;
-    }
-  };
-
-  myNavBar.init([
-    "header",
-    "header-container",
-    "brand"
-  ]);
-
-  function offSetManager() {
-    var yOffset = 0;
-    var currYOffSet = window.pageYOffset;
-
-    if (yOffset < currYOffSet) {
-      myNavBar.add();
-    } else if (currYOffSet == yOffset) {
-      myNavBar.remove();
-    }
-  }
-
-  window.onscroll = function(e) {
-    offSetManager();
-  }
-
-  offSetManager();
-});
+// $(document).ready(function() {
+//   var myNavBar = {
+//     flagAdd: true,
+//     elements: [],
+//     init: function(elements) {
+//       this.elements = elements;
+//     },
+//     add: function() {
+//       if (this.flagAdd) {
+//         for (var i = 0; i < this.elements.length; i++) {
+//           document.getElementById(this.elements[i]).className += " fixed-theme";
+//         }
+//         this.flagAdd = false;
+//       }
+//     },
+//     remove: function() {
+//       for (var i = 0; i < this.elements.length; i++) {
+//         document.getElementById(this.elements[i]).className =
+//           document.getElementById(this.elements[i]).className.replace(/(?:^|\s)fixed-theme(?!\S)/g, '');
+//       }
+//       this.flagAdd = true;
+//     }
+//   };
+//
+//   myNavBar.init([
+//     "header",
+//     "header-container",
+//     "brand"
+//   ]);
+//
+//   function offSetManager() {
+//     var yOffset = 0;
+//     var currYOffSet = window.pageYOffset;
+//
+//     if (yOffset < currYOffSet) {
+//       myNavBar.add();
+//     } else if (currYOffSet == yOffset) {
+//       myNavBar.remove();
+//     }
+//   }
+//
+//   window.onscroll = function(e) {
+//     offSetManager();
+//   }
+//
+//   offSetManager();
+// });
